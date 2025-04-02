75cb220b3bb062a87fa7146f522eeba6a771718b
Paweł Borowski <p.borowski88@gmail.com>
2025-04-02 07:57:06 +0200

    playground app setup

---
+++ apps/playground/src/app/app.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './app';
+
+describe('App', () => {
+  it('should have a greeting as the title', () => {
+    render(<App />);
+    expect(screen.getByText('Playground App')).toBeInTheDocument();
+  });
+});
---
+++ apps/playground/src/app/app.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return <div>Playground App</div>;
+}
+
+export default App;
